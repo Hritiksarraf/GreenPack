c4c1c6ef393d67de9bbaaf35eb10bca1d5b591f4
hritiksarraf <hritiksarraf@gmail.com>
2024-03-21 19:44:41 +0530

    updated mobile responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -100,7 +100,7 @@ export default function Hero() {
   return (
     <>
       <div style={bgImage} id="home"
-        className=' min-h-[100vh] w-[screen]
+        className=' min-h-[100vh] w-[100vw]
     sm:min-h-[100vh] bg-green-100 dark:bg-gray-950
     dark:text-white duration-200 flex justify-center items-center '
       >
@@ -108,27 +108,27 @@ export default function Hero() {
           <div className='grid grid-cols-1 sm:grid-cols-2 '>
             <div className='flex flex-col justify-center gap-5 pt-12 mt-16 sm:pt-0 text-center sm:text-left order-2 sm:order-1'>
               <h1 className='text-5xl sm:text-6xl lg:text7xl font-bold'><span className='text-green-700'>GreenPack</span> Innovations</h1>
-              <h1 className='text-5xl hidden md:block stikey'>{`Excel in packing and baranding with us`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "papper cups", "", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+              <h1 className='text-5xl hidden sm:mt-0 mt-20 md:block stikey'>{`Excel in packing and baranding with us`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "papper cups", "", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
               <p className='text-sm'>Transforming your visions into vibrant realities, GREENPACK
 INNOVATIONS - where sustainable solutions meet cutting edge
 creativity, redefining packaging excellence.</p>
-              <Link to="#contact" smooth className='flex w-40 py-2 items-center gap-2 text-white bg-gradient-to-r  from-[#74D680] to-[#378B29] py-1 px-4 rounded-full  hover:scale-105 duration-200 text-2xl '>Contact
+              <Link to="#contact" smooth className='flex mt-10 sm:mt-0 w-40 py-2 items-center gap-2 text-white bg-gradient-to-r  from-[#74D680] to-[#378B29] py-1 px-4 rounded-full  hover:scale-105 duration-200 text-2xl '>Contact
               <img width="40" height="40" src="https://img.icons8.com/plasticine/100/phone.png" alt="phone"/>
               </Link>
             </div>
-            <div className='order-1  sm:order-2 relative min-h-[50px] sm:min-h-[450px] flex items-center justify-center '>
+            <div className='order-1  sm:order-2 relative min-h-[50px] sm:min-h-[400px] flex items-center justify-center '>
               <div>
-                <img src={imageId} alt="" className='w-[300px] h-[40vh] p-4 sm:w-[350px] mx-auto spin ' />
+                <img src={imageId} alt="" className='w-[70vw] h-[30vh] sm:h-[40vh] p-4 sm:w-[350px] mx-auto spin ' />
               </div>
-              <di className='flex lg:flex-col lg:top-1/2 lg:-translate-y-1/2 translate-y-[10vh] lg:py-2 justify-center gap-4 md:h-[20rem] absolute bottom-[0px] lg:-right-10 bg-white/30 rounded-full'>
+              <div className='flex lg:flex-col  lg:top-1/2 lg:-translate-y-1/2 translate-y-[10vh] lg:py-2 justify-center gap-4 md:h-[20rem] absolute bottom-[0px] lg:-right-10 bg-white/30 rounded-full'>
                 {imageList.map((item) => (
-                  <img alt="" key={item.id} src={item.img} className='w-[80px] h-[70px] object-contain inline-block  hover:scale-105 duration-200'
+                  <img alt="" key={item.id} src={item.img} className='w-[80px]  h-[70px] object-contain inline-block  hover:scale-105 duration-200'
                     onClick={() => {
                       setImageId(item.img);
                     }}
                   />
                 ))}
-              </di>
+              </div>
 
             </div>
 
